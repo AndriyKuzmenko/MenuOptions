7965fc37fc1477ec49f4a6835177ff59bd1a73e7
Andriy <ak8022@bs.amalnet.k12.il>
2020-10-23 19:15:03 +0300

    Menus activity 2

--- app/src/main/java/com/example/menuoptions/Activity2.java
+++ app/src/main/java/com/example/menuoptions/Activity2.java
@@ -2,8 +2,10 @@ package com.example.menuoptions;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
 
@@ -24,6 +26,30 @@ public class Activity2 extends AppCompatActivity
     public boolean onCreateOptionsMenu(Menu menu)
     {
         getMenuInflater().inflate(R.menu.activity2, menu);
+
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        String name=item.getTitle().toString();
+
+        if (name.equals("Red"))
+        {
+            layout.setBackgroundColor(Color.RED);
+        }
+        else if (name.equals("Green"))
+        {
+            layout.setBackgroundColor(Color.GREEN);
+        }
+        else if (name.equals("Blue"))
+        {
+            layout.setBackgroundColor(Color.BLUE);
+        }
+        else if (name.equals("Yellow"))
+        {
+            layout.setBackgroundColor(Color.YELLOW);
+        }
 
         return true;
     }
