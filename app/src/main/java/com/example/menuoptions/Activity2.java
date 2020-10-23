5413d59ed583a7efb0c531191a746c4e27a1e177
Andriy <ak8022@bs.amalnet.k12.il>
2020-10-23 19:07:52 +0300

    Removed useless ids

--- app/src/main/java/com/example/menuoptions/Activity2.java
+++ app/src/main/java/com/example/menuoptions/Activity2.java
@@ -3,6 +3,7 @@ package com.example.menuoptions;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Activity2 extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class Activity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+    }
+
+    public void buttonPressed(View view) {
     }
 }
