b058aaa5347f2487175fdf50ef1099d3128f9ff3
Andriy <ak8022@bs.amalnet.k12.il>
2020-10-23 19:09:09 +0300

    Ids

--- app/src/main/java/com/example/menuoptions/Activity2.java
+++ app/src/main/java/com/example/menuoptions/Activity2.java
@@ -4,15 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 
-public class Activity2 extends AppCompatActivity {
+public class Activity2 extends AppCompatActivity
+{
+    LinearLayout layout;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+
+        layout=(LinearLayout)findViewById(R.id.layout);
     }
 
-    public void buttonPressed(View view) {
+    public void buttonPressed(View view)
+    {
+
     }
 }
