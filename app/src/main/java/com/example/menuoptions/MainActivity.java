a8d630823d0719c2d49b7239df352f1c854116c3
Andriy <ak8022@bs.amalnet.k12.il>
2020-10-23 18:51:35 +0300

    Changing Colors

--- app/src/main/java/com/example/menuoptions/MainActivity.java
+++ app/src/main/java/com/example/menuoptions/MainActivity.java
@@ -2,23 +2,49 @@ package com.example.menuoptions;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity
 {
+    LinearLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        layout=(LinearLayout)findViewById(R.id.layout);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
         getMenuInflater().inflate(R.menu.main, menu);
+
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        String name=item.getTitle().toString();
+
+        if (name.equals("Red"))
+        {
+            layout.setBackgroundColor(Color.RED);
+        }
+        else if (name.equals("Green"))
+        {
+            layout.setBackgroundColor(Color.GREEN);
+        }
+        else if (name.equals("Blue"))
+        {
+            layout.setBackgroundColor(Color.BLUE);
+        }
 
         return true;
     }
